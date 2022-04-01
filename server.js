83b1fd96186fd80b0a00db487571138443e13b75
Yashar <yasharjs@gmail.com>
2022-04-01 05:21:18 -0400

    dploy try

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/27017/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -17,10 +17,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
+  useFindAndModify: false,
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
+mongoose.set('debug', true);
 // routes
 app.use(require("./routes/api.js"));
 
